e2312caaf2f2fcf30d8388eb576d0d2152f6821b
Varokas Panusuwan <varokas@panusuwan.com>
2014-01-15 17:49:29 -0800

    refactor test

--- spec/CheckAmountSpec.js
+++ spec/CheckAmountSpec.js
@@ -3,18 +3,16 @@
 describe("When CheckAmount is instantiated with ", function () {
 
   describe("only units digit", function () {
-
-      it("should return \"one dollar\" when given value is 1", function () {
-          expect(new CheckAmount("1").toString()).toEqual("one dollar");
-      })
-
-      it("should return \"two dollars\" when given value is 2", function () {
-          expect(new CheckAmount("2").toString()).toEqual("two dollars");
-      })
-
-      it("should return \"three dollars\" when given value is 3", function () {
-          expect(new CheckAmount("3").toString()).toEqual("three dollars");
-      })
+      [
+        { word: "one dollar", number: "1" },
+        { word: "two dollars", number: "2" },
+        { word: "three dollars", number: "3" },
+      ]
+      .forEach( function(e) {
+         it("should return \"" + e.word + "\" when given value is " + e.number, function() {
+            expect(new CheckAmount(e.number).toString()).toEqual(e.word);
+         });
+      });
   });
 
 
