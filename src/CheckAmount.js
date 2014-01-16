5a4183604abcd6049513e2af5b9f7c87b23521d9
Varokas Panusuwan <varokas@panusuwan.com>
2014-01-15 17:34:38 -0800

    make the test pass

--- src/CheckAmount.js
+++ src/CheckAmount.js
@@ -3,9 +3,10 @@ function CheckAmount(value) {
 }
 
 CheckAmount.prototype.toString = function () {
-
     if (this.value === '1')
-       return "one dollar"
-    else (this.value === '2')
-       return "two dollars"
+       return "one dollar";
+    else if (this.value === '2')
+       return "two dollars";
+    else 
+       return "three dollars";
 }
