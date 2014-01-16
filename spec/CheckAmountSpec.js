79934843dcb9f46c4f490222d64f63c6ae0b687e
Varokas Panusuwan <varokas@panusuwan.com>
2014-01-15 17:52:13 -0800

    expand the one digit test case to cover all non zeros digits

--- spec/CheckAmountSpec.js
+++ spec/CheckAmountSpec.js
@@ -7,6 +7,12 @@ describe("When CheckAmount is instantiated with ", function () {
         { word: "one dollar", number: "1" },
         { word: "two dollars", number: "2" },
         { word: "three dollars", number: "3" },
+        { word: "four dollars", number: "4" },
+        { word: "five dollars", number: "5" },
+        { word: "six dollars", number: "6" },
+        { word: "seven dollars", number: "7" },
+        { word: "eight dollars", number: "8" },
+        { word: "nine dollars", number: "9" },
       ]
       .forEach( function(e) {
          it("should return \"" + e.word + "\" when given value is " + e.number, function() {
