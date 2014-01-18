7fd76aae01bff405c6f9b6144a284c8fd8b88bb2
schalermthai <schalermthai@sprint3r.com>
2014-01-19 00:27:50 +0700

    added support for teens number. added more test for tens numbers
    - Introduce private variable & method

--- src/CheckAmount.js
+++ src/CheckAmount.js
@@ -1,37 +1,42 @@
-function CheckAmount(value) {
-    this.value = value;
-}
+CheckAmount = (function() {
 
-CheckAmount.prototype.IsCurrencyNeedToBePluralrized = function () {
-
-    return this.value != '1';
-
-}
-
-CheckAmount.prototype.GetCurrency = function () {
-   
-    var currency = "dollar";
-
-    if (this.IsCurrencyNeedToBePluralrized()) {
-        currency += "s";
+    function CheckAmount(value) {
+        this.value = value;
     }
-    
-    return currency;
-}
 
-CheckAmount.prototype.toString = function () {
+    var numberText = {
+            '1': 'one',
+            '2': 'two',
+            '3': 'three',
+            '4': 'four',
+            '5': 'five',
+            '6': 'six',
+            '7': 'seven',
+            '8': 'eight',
+            '9': 'nine',
+            '10': 'ten',
+            '11': 'eleven',
+            '12': 'twelve',
+            '13': 'thirteen',
+            '14': 'fourteen',
+            '15': 'fifteen',
+            '16': 'sixteen',
+            '17': 'seventeen',
+            '18': 'eighteen',
+            '19': 'nineteen'
+    }
 
-	var numberText = {
-		'1': 'one',
-		'2': 'two',
-		'3': 'three',
-		'4': 'four',
-		'5': 'five',
-		'6': 'six',
-		'7': 'seven',
-		'8': 'eight',
-		'9': 'nine',
-	}
+    var isOne = function () {
+        return this.value !== '1';
+    }
 
-	return numberText[this.value] +" " + this.GetCurrency();
-}
+    CheckAmount.prototype.GetCurrency = function () {
+        return isOne() ? 'dollars' : 'dollar'
+    }
+
+    CheckAmount.prototype.toString = function () {
+        return numberText[this.value] + " " + this.GetCurrency();
+    }
+
+    return CheckAmount;
+})();
