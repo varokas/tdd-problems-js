d1e0cef70f1c717821a4c24a3615c1d1264a49f0
sarawut-p <sarawut.positwinyu@gmail.com>
2014-01-18 23:29:24 +0700

    make all only units digit pass test
    add failing test for teen numbers

--- spec/CheckAmountSpec.js
+++ spec/CheckAmountSpec.js
@@ -21,5 +21,24 @@ describe("When CheckAmount is instantiated with ", function () {
       });
   });
 
+  describe("teen numbers", function () {
+      [
+        { word: "ten dollars", number: "10" },
+        { word: "eleven dollars", number: "11" },
+        { word: "twelve dollars", number: "12" },
+        { word: "thirteen dollars", number: "13" },
+        { word: "fourteen dollars", number: "14" },
+        { word: "fifteen dollars", number: "15" },
+        { word: "sixteen dollars", number: "16" },
+        { word: "seventeen dollars", number: "17" },
+        { word: "eighteen dollars", number: "18" },
+        { word: "nineteen dollars", number: "19" },
+      ]
+      .forEach(function (e) {
+          it("should return \"" + e.word + "\" when given value is " + e.number, function () {
+              expect(new CheckAmount(e.number).toString()).toEqual(e.word);
+          });
+      });
+  });
 
 });
--- src/CheckAmount.js
+++ src/CheckAmount.js
@@ -2,12 +2,36 @@ function CheckAmount(value) {
     this.value = value;
 }
 
+CheckAmount.prototype.IsCurrencyNeedToBePluralrized = function () {
+
+    return this.value != '1';
+
+}
+
+CheckAmount.prototype.GetCurrency = function () {
+   
+    var currency = "dollar";
+
+    if (this.IsCurrencyNeedToBePluralrized()) {
+        currency += "s";
+    }
+    
+    return currency;
+}
+
 CheckAmount.prototype.toString = function () {
+
 	var numberText = {
-		'1': 'one dollar',
-		'2': 'two dollars',
-		'3': 'three dollars'
+		'1': 'one',
+		'2': 'two',
+		'3': 'three',
+		'4': 'four',
+		'5': 'five',
+		'6': 'six',
+		'7': 'seven',
+		'8': 'eight',
+		'9': 'nine',
 	}
 
-	return numberText[this.value]
+	return numberText[this.value] +" " + this.GetCurrency();
 }
