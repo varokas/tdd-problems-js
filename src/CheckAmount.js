080c44ca6e57e484877c0b8b13fe78463a092deb
Varokas Panusuwan <varokas@panusuwan.com>
2014-01-18 22:14:42 -0800

    refactor

--- src/CheckAmount.js
+++ src/CheckAmount.js
@@ -6,39 +6,6 @@ CheckAmount = (function() {
         value = val;
     }
 
-    var numberText = {
-            1: 'one',
-            2: 'two',
-            3: 'three',
-            4: 'four',
-            5: 'five',
-            6: 'six',
-            7: 'seven',
-            8: 'eight',
-            9: 'nine',
-            10: 'ten',
-            11: 'eleven',
-            12: 'twelve',
-            13: 'thirteen',
-            14: 'fourteen',
-            15: 'fifteen',
-            16: 'sixteen',
-            17: 'seventeen',
-            18: 'eighteen',
-            19: 'nineteen'
-    }
-
-    var tensText = {
-    	2: "twenty",
-    	3: "thirty",
-    	4: "fourty",
-    	5: "fifty",
-    	6: "sixty",
-    	7: "seventy",
-    	8: "eighty",
-    	9: "ninety",
-    }
-
     var isOne = function (number) {
         return number === 1;
     }
@@ -49,23 +16,71 @@ CheckAmount = (function() {
 
     var getNumberText = function(number) {
     	var resultText = "";
+    	var context = {"number": number};
 
-    	if(number >= 20) {
-    		resultText += tensText[parseInt(number/10)]
+    	var filters = [tensFilter, digitFilter];
 
-    		number = number - parseInt(number/10) * 10
-    		if(number > 0) {
-    			resultText += " ";
+    	filters.forEach(function(filter) {
+    		var text = filter(context);
+    		if(text != "") {
+    			resultText += (text + " ");
     		}
-    	}
-
-    	resultText += number != 0 ? numberText[number] : "";
+    	});
 
     	return resultText;
     }
 
+    var tensFilter = function(context) {
+    	var text = "";
+
+    	if(context.number >= 20) {
+		    var tensText = {
+		    	2: "twenty",
+		    	3: "thirty",
+		    	4: "fourty",
+		    	5: "fifty",
+		    	6: "sixty",
+		    	7: "seventy",
+		    	8: "eighty",
+		    	9: "ninety",
+		    }
+
+    	    text = tensText[parseInt(context.number/10)]
+    		context.number -= parseInt(context.number/10) * 10
+    	}
+
+    	return text; 
+    }
+
+    var digitFilter = function(context) {
+	    var numberText = {
+	    		0: '',
+	            1: 'one',
+	            2: 'two',
+	            3: 'three',
+	            4: 'four',
+	            5: 'five',
+	            6: 'six',
+	            7: 'seven',
+	            8: 'eight',
+	            9: 'nine',
+	            10: 'ten',
+	            11: 'eleven',
+	            12: 'twelve',
+	            13: 'thirteen',
+	            14: 'fourteen',
+	            15: 'fifteen',
+	            16: 'sixteen',
+	            17: 'seventeen',
+	            18: 'eighteen',
+	            19: 'nineteen'
+	    }
+
+   		return numberText[context.number]; 	
+    }
+
     CheckAmount.prototype.toString = function () {
-        return getNumberText(value) + " " + getCurrency(value);
+        return getNumberText(value) + getCurrency(value);
     }
 
     return CheckAmount;
