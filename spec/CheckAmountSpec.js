729749fbbb5946437a71b008afb2a6a4564d0d73
Varokas Panusuwan <varokas@panusuwan.com>
2014-01-18 21:31:57 -0800

    use proper number types

--- spec/CheckAmountSpec.js
+++ spec/CheckAmountSpec.js
@@ -4,15 +4,15 @@ describe("When CheckAmount is instantiated with ", function () {
 
   describe("only units digit", function () {
       [
-        { word: "one dollar", number: "1" },
-        { word: "two dollars", number: "2" },
-        { word: "three dollars", number: "3" },
-        { word: "four dollars", number: "4" },
-        { word: "five dollars", number: "5" },
-        { word: "six dollars", number: "6" },
-        { word: "seven dollars", number: "7" },
-        { word: "eight dollars", number: "8" },
-        { word: "nine dollars", number: "9" },
+        { word: "one dollar", number: 1 },
+        { word: "two dollars", number: 2 },
+        { word: "three dollars", number: 3 },
+        { word: "four dollars", number: 4 },
+        { word: "five dollars", number: 5 },
+        { word: "six dollars", number: 6 },
+        { word: "seven dollars", number: 7 },
+        { word: "eight dollars", number: 8 },
+        { word: "nine dollars", number: 9 },
       ]
       .forEach( function(e) {
          it("should return \"" + e.word + "\" when given value is " + e.number, function() {
@@ -23,16 +23,16 @@ describe("When CheckAmount is instantiated with ", function () {
 
   describe("teen numbers", function () {
       [
-        { word: "ten dollars", number: "10" },
-        { word: "eleven dollars", number: "11" },
-        { word: "twelve dollars", number: "12" },
-        { word: "thirteen dollars", number: "13" },
-        { word: "fourteen dollars", number: "14" },
-        { word: "fifteen dollars", number: "15" },
-        { word: "sixteen dollars", number: "16" },
-        { word: "seventeen dollars", number: "17" },
-        { word: "eighteen dollars", number: "18" },
-        { word: "nineteen dollars", number: "19" },
+        { word: "ten dollars", number: 10 },
+        { word: "eleven dollars", number: 11 },
+        { word: "twelve dollars", number: 12 },
+        { word: "thirteen dollars", number: 13 },
+        { word: "fourteen dollars", number: 14 },
+        { word: "fifteen dollars", number: 15 },
+        { word: "sixteen dollars", number: 16 },
+        { word: "seventeen dollars", number: 17 },
+        { word: "eighteen dollars", number: 18 },
+        { word: "nineteen dollars", number: 19 },
       ]
       .forEach(function (e) {
           it("should return \"" + e.word + "\" when given value is " + e.number, function () {
@@ -43,16 +43,16 @@ describe("When CheckAmount is instantiated with ", function () {
 
     describe("other ten numbers", function () {
       [
-        { word: "twenty dollars", number: "20" },
-        { word: "thirty one dollars", number: "31" },
-        { word: "fourty two dollars", number: "42" },
-        { word: "fifty three dollars", number: "53" },
-        { word: "sixty four dollars", number: "64" },
-        { word: "seventy five dollars", number: "75" },
-        { word: "eighty six dollars", number: "86" },
-        { word: "ninety seven dollars", number: "97" },
-        { word: "twenty eight dollars", number: "28" },
-        { word: "thirty nine dollars", number: "39" },
+        { word: "twenty dollars", number: 20 },
+        { word: "thirty one dollars", number: 31 },
+        { word: "fourty two dollars", number: 42 },
+        { word: "fifty three dollars", number: 53 },
+        { word: "sixty four dollars", number: 64 },
+        { word: "seventy five dollars", number: 75 },
+        { word: "eighty six dollars", number: 86 },
+        { word: "ninety seven dollars", number: 97 },
+        { word: "twenty eight dollars", number: 28 },
+        { word: "thirty nine dollars", number: 39 },
       ]
       .forEach(function (e) {
           it("should return \"" + e.word + "\" when given value is " + e.number, function () {
--- src/CheckAmount.js
+++ src/CheckAmount.js
@@ -7,29 +7,29 @@ CheckAmount = (function() {
     }
 
     var numberText = {
-            '1': 'one',
-            '2': 'two',
-            '3': 'three',
-            '4': 'four',
-            '5': 'five',
-            '6': 'six',
-            '7': 'seven',
-            '8': 'eight',
-            '9': 'nine',
-            '10': 'ten',
-            '11': 'eleven',
-            '12': 'twelve',
-            '13': 'thirteen',
-            '14': 'fourteen',
-            '15': 'fifteen',
-            '16': 'sixteen',
-            '17': 'seventeen',
-            '18': 'eighteen',
-            '19': 'nineteen'
+            1: 'one',
+            2: 'two',
+            3: 'three',
+            4: 'four',
+            5: 'five',
+            6: 'six',
+            7: 'seven',
+            8: 'eight',
+            9: 'nine',
+            10: 'ten',
+            11: 'eleven',
+            12: 'twelve',
+            13: 'thirteen',
+            14: 'fourteen',
+            15: 'fifteen',
+            16: 'sixteen',
+            17: 'seventeen',
+            18: 'eighteen',
+            19: 'nineteen'
     }
 
     var isOne = function () {
-        return value === '1';
+        return value === 1;
     }
 
     var getCurrency = function () {
