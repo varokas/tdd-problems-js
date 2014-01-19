845d9686b28c52d92e1682211390e0994f36f28c
Varokas Panusuwan <varokas@panusuwan.com>
2014-01-18 21:54:11 -0800

    make test pass

--- src/CheckAmount.js
+++ src/CheckAmount.js
@@ -28,16 +28,44 @@ CheckAmount = (function() {
             19: 'nineteen'
     }
 
-    var isOne = function () {
-        return value === 1;
+    var tensText = {
+    	2: "twenty",
+    	3: "thirty",
+    	4: "fourty",
+    	5: "fifty",
+    	6: "sixty",
+    	7: "seventy",
+    	8: "eighty",
+    	9: "ninety",
     }
 
-    var getCurrency = function () {
-        return isOne() ? 'dollar' : 'dollars'
+    var isOne = function (number) {
+        return number === 1;
+    }
+
+    var getCurrency = function (number) {
+        return isOne(number) ? 'dollar' : 'dollars'
+    }
+
+    var getNumberText = function(number) {
+    	var resultText = "";
+
+    	if(number >= 20) {
+    		resultText += tensText[parseInt(number/10)]
+
+    		number = number - parseInt(number/10) * 10
+    		if(number > 0) {
+    			resultText += " ";
+    		}
+    	}
+
+    	resultText += number != 0 ? numberText[number] : "";
+
+    	return resultText;
     }
 
     CheckAmount.prototype.toString = function () {
-        return numberText[value] + " " + getCurrency();
+        return getNumberText(value) + " " + getCurrency(value);
     }
 
     return CheckAmount;
