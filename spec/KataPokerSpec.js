6fd8a5c192d697781f43bbd2c83296cccd6b7746
armeo <nicao11@gmail.com>
2014-01-23 20:02:33 +0700

    - fixed test show highest - refactor Player - add one test fail

--- spec/KataPokerSpec.js
+++ spec/KataPokerSpec.js
@@ -1,33 +1,32 @@
 describe("PokerGame", function () {
 
     describe("Cards", function () {
-
         it("with same rank but different suites should score equally the same", function() {
-          expect(new Card('A', 'C').score()).toEqual(new Card('A', 'D').score());
-          expect(new Card('A', 'D').score()).toEqual(new Card('A', 'H').score());
-          expect(new Card('A', 'H').score()).toEqual(new Card('A', 'S').score());
+            expect(new Card('A', 'C').score()).toEqual(new Card('A', 'D').score());
+            expect(new Card('A', 'D').score()).toEqual(new Card('A', 'H').score());
+            expect(new Card('A', 'H').score()).toEqual(new Card('A', 'S').score());
         });
 
         describe("should score according to their rank.", function () {
-        [
-          {rank : '2',  suite : 'C', score : 2  },
-          {rank : '3',  suite : 'C', score : 3  },
-          {rank : '4',  suite : 'C', score : 4  },
-          {rank : '5',  suite : 'C', score : 5  },
-          {rank : '6',  suite : 'C', score : 6  },
-          {rank : '7',  suite : 'C', score : 7  },
-          {rank : '8',  suite : 'C', score : 8  },
-          {rank : '9',  suite : 'C', score : 9  },
-          {rank : 'T', suite : 'C', score : 10 },
-          {rank : 'J',  suite : 'C', score : 11 },
-          {rank : 'Q',  suite : 'C', score : 12 },
-          {rank : 'K',  suite : 'C', score : 13 },
-          {rank : 'A',  suite : 'C', score : 14 },
-        ].forEach(function(e) {
-              it("A card of " + e.rank + e.suite + " should score " + e.score + " points", function() {
-                  expect(new Card(e.rank, e.suite).score()).toEqual(e.score);
-              });
-          });
+            [
+                {rank : '2',  suite : 'C', score : 2  },
+                {rank : '3',  suite : 'C', score : 3  },
+                {rank : '4',  suite : 'C', score : 4  },
+                {rank : '5',  suite : 'C', score : 5  },
+                {rank : '6',  suite : 'C', score : 6  },
+                {rank : '7',  suite : 'C', score : 7  },
+                {rank : '8',  suite : 'C', score : 8  },
+                {rank : '9',  suite : 'C', score : 9  },
+                {rank : 'T',  suite : 'C', score : 10 },
+                {rank : 'J',  suite : 'C', score : 11 },
+                {rank : 'Q',  suite : 'C', score : 12 },
+                {rank : 'K',  suite : 'C', score : 13 },
+                {rank : 'A',  suite : 'C', score : 14 },
+            ].forEach(function(e) {
+                it("A card of " + e.rank + e.suite + " should score " + e.score + " points", function() {
+                    expect(new Card(e.rank, e.suite).score()).toEqual(e.score);
+                });
+            });
         });
 
         it("can create card from shorthand notation", function() {
@@ -42,6 +41,11 @@ describe("PokerGame", function () {
             var black = new Player('Black', ['2H', '3D', '5S', '9C', 'KD']);
             expect(black.nextHighest()).toEqual(13);
             expect(black.nextHighest()).toEqual(9);
+        });
+
+        it("can show flush in player hand", function() {
+            var white = new Player('White', ['2S', '8S', 'AS', 'QS', '3S']);
+            expect(white.rankOnHand()).toEqual('Flush');
         });
     });
 });
--- src/poker/KataPoker.js
+++ src/poker/KataPoker.js
@@ -1,4 +1,14 @@
+var CardFactory = {
+    createByCode: function(code) {
+        return new Card(code[0], code[1]);
+    }
+};
+
 Card = (function() {
+    function Card(_rank, _suite) {
+        this.rank  = _rank;
+        this.suite = _suite;
+    }
 
     var _getScoreByRank = function (_rank) {
         switch(_rank) {
@@ -11,30 +21,31 @@ Card = (function() {
         }
     };
 
-    function Card(_rank, _suite) {
-        this.rank  = _rank;
-        this.suite = _suite;
-    }
-
     Card.prototype.score = function() {
-       return _getScoreByRank(this.rank);
+        return _getScoreByRank(this.rank);
     };
 
     return Card;
 })();
 
-var CardFactory = {
-    createByCode: function(code) {
-        return new Card(code[0], code[1]);
-    }
-};
+Player = (function() {
+    function Player(_name, _cards) {
+        this.name  = _name;
+        this.cards = _cards;
 
-Player = function Player(_name, _cards) {
-    this.name  = _name;
-    this.cards = _cards;
-};
+        this.onHandRanks = [];
+        for(card in this.cards)
+            this.onHandRanks.push(CardFactory.createByCode(this.cards[card]).score());
+        this.onHandRanks.reverse();
+    };
 
-Player.prototype.nextHighest = function() {
-   return 0;
-};
+    Player.prototype.nextHighest = function() {
+        highest = this.onHandRanks[0]
+        this.onHandRanks.shift()
+
+        return highest;
+    };
+
+    return Player;
+})();
 
