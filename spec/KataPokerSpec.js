b38e54bc7921b3fe6606031765b788fe30f8a98d
biiterlove <peerawat.pm@@gmail.com>
2014-01-23 11:37:33 +0700

    clean up code

--- spec/KataPokerSpec.js
+++ spec/KataPokerSpec.js
@@ -2,41 +2,38 @@ describe("PokerGame", function () {
 
     describe("Cards", function () {
 
-		it("with same rank but different suites should score equally the same", function() {
-		    expect(new Card('A', 'C').score()).toEqual(new Card('A', 'D').score());
-		    expect(new Card('A', 'D').score()).toEqual(new Card('A', 'H').score());
-		    expect(new Card('A', 'H').score()).toEqual(new Card('A', 'S').score());
-		});
+        it("with same rank but different suites should score equally the same", function() {
+          expect(new Card('A', 'C').score()).toEqual(new Card('A', 'D').score());
+          expect(new Card('A', 'D').score()).toEqual(new Card('A', 'H').score());
+          expect(new Card('A', 'H').score()).toEqual(new Card('A', 'S').score());
+        });
 
-		describe("should score according to their rank.", function () {
-		    [
-			{rank : '2',  suite : 'C', score : 2  },
-			{rank : '3',  suite : 'C', score : 3  },
-			{rank : '4',  suite : 'C', score : 4  },
-			{rank : '5',  suite : 'C', score : 5  },
-			{rank : '6',  suite : 'C', score : 6  },
-			{rank : '7',  suite : 'C', score : 7  },
-			{rank : '8',  suite : 'C', score : 8  },
-			{rank : '9',  suite : 'C', score : 9  },
-			{rank : 'T', suite : 'C', score : 10 },
-			{rank : 'J',  suite : 'C', score : 11 },
-			{rank : 'Q',  suite : 'C', score : 12 },
-			{rank : 'K',  suite : 'C', score : 13 },
-			{rank : 'A',  suite : 'C', score : 14 },
-		    ]
-		    .forEach(function(e) {
-			it("A card of " + e.rank + e.suite + " should score " + e.score + " points", function() {
-			    expect(new Card(e.rank, e.suite).score()).toEqual(e.score);
-			});
-		    });
-		});
+        describe("should score according to their rank.", function () {
+        [
+          {rank : '2',  suite : 'C', score : 2  },
+          {rank : '3',  suite : 'C', score : 3  },
+          {rank : '4',  suite : 'C', score : 4  },
+          {rank : '5',  suite : 'C', score : 5  },
+          {rank : '6',  suite : 'C', score : 6  },
+          {rank : '7',  suite : 'C', score : 7  },
+          {rank : '8',  suite : 'C', score : 8  },
+          {rank : '9',  suite : 'C', score : 9  },
+          {rank : 'T', suite : 'C', score : 10 },
+          {rank : 'J',  suite : 'C', score : 11 },
+          {rank : 'Q',  suite : 'C', score : 12 },
+          {rank : 'K',  suite : 'C', score : 13 },
+          {rank : 'A',  suite : 'C', score : 14 },
+        ].forEach(function(e) {
+              it("A card of " + e.rank + e.suite + " should score " + e.score + " points", function() {
+                  expect(new Card(e.rank, e.suite).score()).toEqual(e.score);
+              });
+          });
+        });
 
-		it("can create card from shorthand notation", function() {
-			var card = CardFactory.createByCode("TD");
-
-			expect(card.rank).toEqual('T');
-			expect(card.suite).toEqual('D');
-		});
-
+        it("can create card from shorthand notation", function() {
+            var card = CardFactory.createByCode("TD");
+            expect(card.rank).toEqual('T');
+            expect(card.suite).toEqual('D');
+        });
     });
 });
--- src/poker/KataPoker.js
+++ src/poker/KataPoker.js
@@ -1,27 +1,26 @@
 Card = (function() {
 
-    var rank;
-    var suite;
+    var rank, suite;
 
-    function Card(_rank, _suite) {
-	rank  = _rank
-	suite = _suite
-    }
-
-    Card.prototype.score = function() {
-	   return _getScoreByRank(rank);
-    }
-
-    function _getScoreByRank(_rank) {
+    var _getScoreByRank = function (_rank) {
         switch(_rank) {
             case 'A': return 14;
             case 'K': return 13;
             case 'Q': return 12;
             case 'J': return 11;
             case 'T': return 10;
-            default: return parseInt(_rank);
+            default: return parseInt(_rank, 10);
         }
+    };
+
+    function Card(_rank, _suite) {
+        rank  = _rank;
+        suite = _suite;
     }
+
+    Card.prototype.score = function() {
+       return _getScoreByRank(rank);
+    };
 
     return Card;
 })();
@@ -30,5 +29,5 @@ var CardFactory = {
     createByCode: function(code) {
         return null;
     }
-}
+};
 
