7dcdbbc0c4c8ba3deb6b6e8271014195c29acf60
schalermthai <schalermthai@sprint3r.com>
2014-02-02 16:30:45 +0700

    format code

--- src/CheckAmount.js
+++ src/CheckAmount.js
@@ -1,6 +1,40 @@
-CheckAmount = (function() {
+var CheckAmount = (function() {
 
     var value;
+
+    var tensText = {
+        2: "twenty",
+        3: "thirty",
+        4: "fourty",
+        5: "fifty",
+        6: "sixty",
+        7: "seventy",
+        8: "eighty",
+        9: "ninety",
+    };
+
+    var singleUnitText = {
+        0: '',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen'
+    };
 
     function CheckAmount(val) {
         value = val;
@@ -8,85 +42,48 @@ CheckAmount = (function() {
 
     var isOne = function (number) {
         return number === 1;
-    }
+    };
 
     var getCurrency = function (number) {
         return isOne(number) ? 'dollar' : 'dollars';
-    }
+    };
 
     var getNumberText = function(number) {
-        var context = {"number": number};
+        var resultText = millionFilter(number);
+        return resultText.flatten().filter(function(e) { return e !== ''; }).join(' ');
+    };
 
-        var resultText = millionFilter(context);
-        return resultText.flatten().filter(function(e) { return e != '' }).join(' ');
-    }
+    var millionFilter = function(number) {
+        var million = parseInt(number / 1000000);
+        return (million) ? [getNumberText(million), "million", thousandFilter(number % 1000000)]
+                         : [thousandFilter(number % 1000000)];
+    };
 
-    var millionFilter = function(context) {
-        var million = parseInt(context.number / 1000000);
-        context.number = context.number % 1000000;
+    var thousandFilter = function(number) {
+        var thousand = parseInt(number / 1000);
+        return (thousand) ? [getNumberText(thousand), "thousand", hundredsFilter(number % 1000)]
+                          : [hundredsFilter(number % 1000)];
+    };
 
-        return (million) ? [getNumberText(million), "million", thousandFilter(context)] : [thousandFilter(context)];
-    }
+    var hundredsFilter = function(number) {
+        var hundred = parseInt(number / 100);
+        return (hundred) ? [getNumberText(hundred), "hundred", tensFilter(number % 100)]
+                         : [tensFilter(number % 100)];
+    };
 
-    var thousandFilter = function(context) {
-        var thousand = parseInt(context.number / 1000);
-        context.number = context.number % 1000;
+    var tensFilter = function(number) {
+        var tens = parseInt(number / 10);
+        return (tens > 1) ? [tensText[tens], digitFilter(number % 10)]
+                          : [digitFilter(number)];
+    };
 
-        return (thousand) ? [getNumberText(thousand), "thousand", hundredsFilter(context)] : [hundredsFilter(context)]
-    }
-
-    var hundredsFilter = function(context) {
-        var hundred = parseInt(context.number / 100);
-        context.number = context.number % 100;
-
-        return (hundred) ? [getNumberText(hundred), "hundred", tensFilter(context)] : [tensFilter(context)]
-    }
-
-    var tensFilter = function(context) {
-        var tensText = {
-            2: "twenty",
-            3: "thirty",
-            4: "fourty",
-            5: "fifty",
-            6: "sixty",
-            7: "seventy",
-            8: "eighty",
-            9: "ninety",
-        };
-
-        return (context.number >= 20) ? [tensText[parseInt(context.number / 10)], digitFilter({number : context.number % 10})] : [digitFilter(context)];
-    }
-
-    var digitFilter = function(context) {
-        var numberText = {
-            0: '',
-            1: 'one',
-            2: 'two',
-            3: 'three',
-            4: 'four',
-            5: 'five',
-            6: 'six',
-            7: 'seven',
-            8: 'eight',
-            9: 'nine',
-            10: 'ten',
-            11: 'eleven',
-            12: 'twelve',
-            13: 'thirteen',
-            14: 'fourteen',
-            15: 'fifteen',
-            16: 'sixteen',
-            17: 'seventeen',
-            18: 'eighteen',
-            19: 'nineteen'
-        };
-
-        return [numberText[context.number]];
-    }
+    var digitFilter = function(number) {
+        return [singleUnitText[number]];
+    };
 
     CheckAmount.prototype.toString = function () {
         return getNumberText(value) + ' ' + getCurrency(value);
-    }
+    };
 
     return CheckAmount;
 })();
